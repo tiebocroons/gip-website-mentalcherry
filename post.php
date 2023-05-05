b9f94725af3e8dd95f89fbf2910381b7de74d71f
HyphaSlayer <tiebo.croons@gmail.com>
2023-05-05 15:10:03 +0200

    Post php added

---
+++ post.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  $to = "Tiebo.croons@gmail.com";
+  $headers = "From: " . $email;
+  $body = "Naam: " . $name . "\nE-mail: " . $email . "\nOnderwerp: " . $subject . "\nBericht: " . $message;
+
+  mail($to, $subject, $body, $headers);
+}
+?>
