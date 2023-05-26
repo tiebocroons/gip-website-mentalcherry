a9b12fe0b5e4b1a9fca0cad71c4bbc9c0fd6d1f0
HyphaSlayer <tiebo.croons@gmail.com>
2023-05-26 09:50:13 +0200

    update

--- post.php
+++ post.php
@@ -9,6 +9,10 @@ if (isset($_POST['submit'])) {
   $headers = "From: " . $email;
   $body = "Naam: " . $name . "\nE-mail: " . $email . "\nOnderwerp: " . $subject . "\nBericht: " . $message;
 
-  mail($to, $subject, $body, $headers);
-}
+  mail($to, $subject, $body, $headers)  {
+    echo "Bedankt voor uw bericht. We hebben uw e-mail ontvangen en nemen zo spoedig mogelijk contact met u op.";
+  } else {
+    echo "Er is een probleem opgetreden bij het versturen van uw e-mail. Probeer het later opnieuw.";
+  }
+ }
 ?>
